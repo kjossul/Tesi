39cc7d80f3249353a05e9d1ead2dfe55256f85a4
Riccardo Novic <riccardo.novic@hotmail.it>
2021-04-18 20:27:38 +0200

    DB fixes

--- data_merger.py
+++ data_merger.py
@@ -1,20 +1,27 @@
 import pandas as pd
 import sys
 import datetime 
-  
+
 # date range
-start = datetime.datetime(2010, 1, 1)
+start_year = 2012
+start = datetime.datetime(start_year, 1, 1)
 gen = ((start + i * datetime.timedelta(minutes=5)).strftime("%Y-%m-%d %H:%M:%S") for i in range(int(12*24*365.25*4)))
 timestamps = pd.DataFrame(gen, columns=["timestamp"])
-# reading csv files
-wheather = pd.read_csv(sys.argv[1])
-wheather = wheather[["timestamp", "Wind_Speed", "Weather_Temperature_Celsius", "Weather_Relative_Humidity", 
-                    "Global_Horizontal_Radiation", "Diffuse_Horizontal_Radiation", "Wind_Direction"]]
-station = pd.read_csv(sys.argv[2])
+# load dataset and merge
+df = pd.read_csv("101-Site_DKA-WeatherStation.csv")
+df.drop(['Wind_Direction', 'Wind_Speed'], axis=1, inplace=True)
+station = pd.read_csv(sys.argv[1])
 station = station[["timestamp", "Active_Power"]]
-# merge dataframes
-merged = pd.merge(timestamps, wheather, on='timestamp', how='outer')     
-merged = pd.merge(merged, station, on='timestamp', how='outer')
-merged = merged[(merged['timestamp'].str.slice(0, 4) >= '2010') & (merged['timestamp'].str.slice(0, 4) < '2014')]
-assert(len(merged) == len(timestamps))              
-merged.to_csv(f"{sys.argv[2]}_merged.csv.zip", index=False, compression='zip')
+df = timestamps.merge(df, on="timestamp", how='outer')
+df = df.merge(station, on="timestamp", how='outer')
+df = df[(df['timestamp'].str.slice(0, 4) >= str(start_year)) 
+        & (df['timestamp'].str.slice(0, 4) < str(start_year + 4))]
+# feature engineering
+date_time = pd.to_datetime(df.pop('timestamp'), format='%Y-%m-%d %H:%M:%S')
+df["Weather_Temperature_Kelvin"] = df.pop("Weather_Temperature_Celsius").apply(lambda c: c + 273.15)
+df = df.mask(df < 0)
+df = df.mask((df < df.quantile(0.05)) | (df > df.quantile(0.95)))
+df = df.interpolate(method='linear', axis=0).ffill().bfill()
+
+assert(len(df) == len(timestamps))              
+df.to_csv(f"{sys.argv[1]}_merged.csv.zip", index=False, compression='zip')
