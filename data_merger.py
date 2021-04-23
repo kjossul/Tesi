f15bb97e3a9a5e7f2b279a609c0b18061d7c926b
Riccardo Novic <riccardo.novic@hotmail.it>
2021-04-23 17:24:46 +0200

    added new files and notebook

--- data_merger.py
+++ data_merger.py
@@ -1,47 +1,51 @@
 import pandas as pd
-import numpy as np
 import sys
-import datetime 
 
 # date range
-start_year = 2014
-years_in_dataset = 4
-def open_csv():
-    start = datetime.datetime(start_year, 1, 1)
-    gen = ((start + i * datetime.timedelta(minutes=5)).strftime("%Y-%m-%d %H:%M:%S") for i in range(int(12*24*365.25*years_in_dataset)))
-    timestamps = pd.DataFrame(gen, columns=["timestamp"])
-    # load dataset and merge
-    df = pd.read_csv("101-Site_DKA-WeatherStation.csv")
-    df.drop(['Wind_Direction', 'Wind_Speed', 'Radiation_Global_Tilted', 'Radiation_Diffuse_Tilted'], axis=1, inplace=True)
-    station = pd.read_csv(sys.argv[1])
+start_year = 2010
+training_years = 3
+test_year = 2020
+
+
+def read_csv(filename):
+    weather = pd.read_csv("101-Site_DKA-WeatherStation.zip")
+    weather.drop(['Wind_Direction', 'Wind_Speed', 'Radiation_Global_Tilted', 'Radiation_Diffuse_Tilted'], axis=1,
+                 inplace=True)
+    station = pd.read_csv(filename)
     station = station[["timestamp", "Active_Power"]]
-    df = timestamps.merge(df, on="timestamp", how='outer')
-    df = df.merge(station, on="timestamp", how='outer')
-    df = df[(df['timestamp'].str.slice(0, 4) >= str(start_year)) 
-            & (df['timestamp'].str.slice(0, 4) < str(start_year + 4))]
+    df = weather.merge(station, on="timestamp", how='outer')
+    df = df[df['timestamp'].str.slice(0, 4)
+        .isin([str(y) for y in range(start_year, start_year + training_years)] + [str(test_year)])]
+    print("applying timestamp indexing")
+    df['timestamp'] = df['timestamp'].apply(lambda s: pd.to_datetime(s, format='%Y-%m-%d %H:%M:%S'))
+    df.set_index('timestamp', inplace=True)
+    df.sort_index(inplace=True)
     return df
-    
-def drop_problematic_days(df, threshold=10):
-    total_days = len(df) / (12 * 24)
-    # keeps only days where at least threshold days are not null
-    filtered = [chunk for chunk in np.array_split(df, total_days) if chunk.shape[0] - chunk.dropna().shape[0] < threshold]
-    return pd.DataFrame(np.concatenate(filtered), columns=df.columns.values)
+
+
+def drop_null_days(df):
+    return pd.concat([group for _, group in df.groupby([df.index.year, df.index.month, df.index.day])
+                      if group.dropna().shape[0] == 12 * 24])
+
 
 def feature_engineering(df):
-    timestamps = pd.to_datetime(df.pop('timestamp'), format='%Y-%m-%d %H:%M:%S')
     df["Weather_Temperature_Kelvin"] = df.pop("Weather_Temperature_Celsius").apply(lambda c: c + 273.15)
     df = df.mask(df < 0)
     df = df.mask((df < df.quantile(0.05)) | (df > df.quantile(0.95)))
     df = df.interpolate(method='linear', axis=0).ffill().bfill()
-    assert(len(df) == len(timestamps))   
-    df['timestamp'] = timestamps
     return df
 
-def save_to_csv(df):    
-    df.to_csv(f"{sys.argv[1]}_merged.csv.zip", index=False, compression='zip')
-    
+
+def save_to_csv(df, filename):
+    df.to_csv(f"{filename}_merged.csv.zip", index=False, compression='zip')
+
+
 if __name__ == '__main__':
-    df = open_csv()
-    df = drop_problematic_days(df)
+    try:
+        filename = sys.argv[1]
+    except IndexError:
+        filename = "93-Site_8-Kaneka.zip"
+    df = read_csv(filename)
+    df = drop_null_days(df)
     df = feature_engineering(df)
-    save_to_csv(df)
+    save_to_csv(df, filename)
