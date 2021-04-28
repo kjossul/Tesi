91d82940f67e088b3d55bb75174b3c2ceba0a9ad
Riccardo Novic <riccardo.novic@hotmail.it>
2021-04-28 11:31:27 +0200

    Added site with high power decay

--- data_merger.py
+++ data_merger.py
@@ -2,23 +2,22 @@ import pandas as pd
 import sys
 
 # date range
-start_year = 2010
+start_year = 2009
 training_years = 3
 test_year = 2020
 
 
 def read_csv(filename):
     weather = pd.read_csv("101-Site_DKA-WeatherStation.zip")
-    weather.drop(['Wind_Direction', 'Wind_Speed', 'Radiation_Global_Tilted', 'Radiation_Diffuse_Tilted'], axis=1,
-                 inplace=True)
+    weather.drop(['Wind_Direction', 'Wind_Speed', 'Radiation_Global_Tilted', 'Radiation_Diffuse_Tilted',
+                  'Weather_Relative_Humidity'],
+                 axis=1, inplace=True)
     station = pd.read_csv(filename)
     station = station[["timestamp", "Active_Power"]]
     df = weather.merge(station, on="timestamp", how='outer')
-    df = df[df['timestamp'].str.slice(0, 4)
-        .isin([str(y) for y in range(start_year, start_year + training_years)] + [str(test_year)])]
-    print("applying timestamp indexing")
-    df['timestamp'] = df['timestamp'].apply(lambda s: pd.to_datetime(s, format='%Y-%m-%d %H:%M:%S'))
+    df['timestamp'] = pd.to_datetime(df['timestamp'], format='%Y-%m-%d %H:%M:%S')
     df.set_index('timestamp', inplace=True)
+    df = df[df.index.year.isin([y for y in range(start_year, start_year + training_years)] + [test_year])]
     df.sort_index(inplace=True)
     return df
 
@@ -44,7 +43,7 @@ if __name__ == '__main__':
     try:
         filename = sys.argv[1]
     except IndexError:
-        filename = "93-Site_8-Kaneka.zip"
+        filename = "95-Site_6-Kyocera.zip"
     df = read_csv(filename)
     df = drop_null_days(df)
     df = feature_engineering(df)
